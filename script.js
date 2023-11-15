e75d23d0f3418c69dd95a48633412bd218ff9bbf
i6Hx <88464178+i6Hx420@users.noreply.github.com>
2023-11-15 04:32:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 function skip() {
-if(document.getElementsByClassName("video-ads")[0].innerHTML !=="") {
-		var banner = false;
-		for(var i = 0; i < document.getElementsByClassName("ytp-ad-overlay-close-button").length; i++) {
-			document.getElementsByClassName("ytp-ad-overlay-close-button")[i].click(); banner = true;
-		}
-		if(banner === false) {
-			document.getElementsByClassName("html5-main-video")[0].currentTime = document.getElementsByClassName("html5-main-video")[0].duration;
+setInterval(() => {
+    if(document.getElementsByClassName("video-ads")[0].innerHTML !=="") {
+        var banner = false;
+        for(var i = 0; i < document.getElementsByClassName("ytp-ad-overlay-close-button").length; i++) {
+            document.getElementsByClassName("ytp-ad-overlay-close-button")[i].click(); banner = true;
+        }
+        if(banner === false) {
+            document.getElementsByClassName("html5-main-video")[0].currentTime = document.getElementsByClassName("html5-main-video")[0].duration;
             try {document.getElementsByClassName("ytp-ad-skip-button")[0].click();}
             catch (err) {document.getElementsByClassName("ytp-ad-skip-button-modern ytp-button")[0].click();}
-		}
-	}
+        }
+    }
+}, 100)
 }
